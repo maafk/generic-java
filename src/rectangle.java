1831d252d99db8a711a7e71e9490a348120c6c8f
Taylor <taylor@taylorondrey.com>
2018-05-20 06:30:46 -0400

    initial commit

---
+++ src/rectangle.java
@@ -0,0 +1,15 @@
+public class Rectangle {
+	public int length;
+	public int width;
+
+	public Rectangle(int length, int width){
+		this.length = length;
+		this.width = width;
+	}
+	public int getArea(){
+		return this.length * this.width;
+	}
+	public int getPerimiter(){
+		return 2 * (this.length + this.width);
+	}
+}
---
+++ src/rectangulator.java
@@ -0,0 +1,11 @@
+public class Rectangulator {
+	public static void main(String[] args) {
+		int length = Integer.parseInt(args[0]);
+		int width = Integer.parseInt(args[1]);
+
+		Rectangle myRectangle = new Rectangle(length, width);
+
+		String output = String.format("*** Your rectangle ***\n\nLength: %d\nWidth: %d\nArea: %d\nPermimiter: %d\n\n", myRectangle.length, myRectangle.width, myRectangle.getArea(), myRectangle.getPerimiter());
+		System.out.println(output);
+	}
+}
